df80b8322c710791214646ddd6e1015fcb540182
draxaris1010 <axel.dezittere@gmail.com>
2019-12-21 17:50:37 +0100

    update

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 #!/usr/bin/node
 const electron = require("electron");
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+	// create the window
+	let win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	
+	// load the *.html file
+	win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
